407566e7c3ae8e8135b4345d211c7e9c88bb47b5
odbartlett <odbartlett1@gmail.com>
2024-11-05 18:30:38 -0500

    Adding a more updated version of these functions to clear my git

---
+++ mission_scripts/full_flight_1.py
@@ -0,0 +1,4 @@
+# Owen Bartlett
+# This is a script designed to mimic a complete flight.
+# It should take off, fly to a predetermined position, stabililze, lower in altitude,
+# then return back to the original gps location. 
